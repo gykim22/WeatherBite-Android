a2231751a47f4e8e4f79aeb91a5ac26216842d1a
KIMGIYUN <k_bright_st@pusan.ac.kr>
2024-11-23 23:41:16 +0900

    Update weatherInfo.kt

--- src/main/java/com/example/termproject/weatherInfo.kt
+++ src/main/java/com/example/termproject/weatherInfo.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface WeatherInterface {
     //기상청 공공 API 단기예보조회를 이용합니다.
-    @GET("getVilageFcst?serviceKey=pzJUu%2FBLvfEQQGS6%2FHpZdFjObtcciRYGqn3y49yu34VMiVpSpSUirszaaMUgcLICVbLrodzZ2fREq7y0CKPKYw%3D%3D")
+    @GET("getVilageFcst?serviceKey=삭제")
 
     fun GetWeather(@Query("dataType") data_type : String,
                    @Query("numOfRows") num_of_rows : Int,
